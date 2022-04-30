dc6571a4f0a5de2c09453e864b08e436bf9efad0
Kleber Petry <kleberpetry@gmail.com>
2022-04-30 13:47:19 -0300

    Update controleEstoque.c

--- controleEstoque.c
+++ controleEstoque.c
@@ -18,25 +18,75 @@ cadastro(){
 	printf("\nDigite o valor do produto: ");
 	scanf("%lf",&produtos[sequencia].valor);
 	printf("\nDigite o estoque do produto: ");
-	printf("%d",&produtos[sequencia].estoque);
+	scanf("%d",&produtos[sequencia].estoque);
 	produtos[sequencia].codigo=sequencia+1;
 	sequencia++;
 }
 entrada(){
-	
+	system("cls");
+	int codigo,achou;
+	printf("\nDigite o código do produto: ");
+	scanf("%d",&codigo);
+	for(cont=0;cont<sequencia;cont++){
+		if(produtos[cont].codigo==codigo){
+			printf("\nEstoque atual: %d",produtos[cont].estoque);
+			printf("\nDigite o valor de entrada: ");
+			int entrada;
+			scanf("%d",&entrada);
+			produtos[cont].estoque+=entrada;
+			printf("\nEstoque atualizado, novo estoque: %d\n",produtos[cont].estoque);
+			achou=1;
+			break;
+		}else{
+			achou=0;
+		}
+	}
+	if(achou==0){
+		printf("\nProduto não encontrado!\n");
+	}
+	system("pause");
 }
 ajuste(){
-	
+	system("cls");
+	int codigo, achou;
+	printf("\nDigite o código do produto: ");
+	scanf("%d",&codigo);
+	for(cont=0;cont<sequencia;cont++){
+		if(produtos[cont].codigo==codigo){
+			printf("\nCódigo: %d",produtos[cont].codigo);
+			printf("\nNome: %s",produtos[cont].nome);
+			printf("\nValor: %0.2lf",produtos[cont].valor);
+			printf("\nDigite o novo nome do produto: ");
+			fflush(stdin);
+			gets(produtos[cont].nome);
+			printf("\nDigite o novo valor do produto: ");
+			scanf("%lf",&produtos[cont].valor);
+			printf("\nCadastro atualizado com sucesso!\n");
+			achou=1;
+			break;
+		}else{
+			achou=0;
+		}
+	}
+	if(achou==0){
+		printf("\nProduto não encontrado!\n");
+	}
+	system("pause");
 }
 listagem(){
 	system("cls");
-	for(cont=0;cont<sequencia;cont++){
-		printf("\n-----------------------------");
-		printf("\nCódigo: %d",produtos[cont].codigo);
-		printf("\nNome: %s",produtos[cont].nome);
-		printf("\nValor: %0.2lf",produtos[cont].valor);
-		printf("\nEstoque: %d",produtos[cont].estoque);
+	if(produtos[0].codigo!=NULL){
+		for(cont=0;cont<sequencia;cont++){
+			printf("\n-----------------------------");
+			printf("\nCódigo: %d",produtos[cont].codigo);
+			printf("\nNome: %s",produtos[cont].nome);
+			printf("\nValor: %0.2lf",produtos[cont].valor);
+			printf("\nEstoque: %d\n",produtos[cont].estoque);
+		}
+	}else{
+		printf("\nSem produtos cadastrados!\n");
 	}
+	
 	system("pause");
 }
 main(){
@@ -52,16 +102,16 @@ main(){
 		scanf("%d",&opcao);
 		switch(opcao){
 			case 1:
-				//cadastro
+				cadastro();
 			break;
 			case 2:
-				//entrada estoque
+				entrada();
 			break;
 			case 3:
-				//alterar estoque
+				ajuste();
 			break;
 			case 4:
-				//listagem dos produtos
+				listagem();
 			break;
 			default:
 				printf("\nOpção inválida!");
